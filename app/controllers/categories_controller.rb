d4bc976430df50bcadf4c371b9f67abf394afc8a
guillermo varela <guillovarela16@gmail.com>
2020-04-14 16:59:05 -0300

    filters added

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,8 @@ class CategoriesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
 
   def show
+    @divisions = Division.all
     @category = Category.find(params[:id])
+    @categories = Category.all
   end
 end
