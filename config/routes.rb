12467483fdfeab0b431f5f382dec27b7a6fd3097
guillermo varela <guillovarela16@gmail.com>
2020-04-14 09:42:29 -0300

    finish the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  get 'variants/new'
-  get 'variants/create'
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/create'
-  get 'products/update'
-  get 'products/edit'
-  get 'products/destroy'
-  get 'categories/show'
   devise_for :users
   root to: 'pages#home'
+  resources :categories, only: :show
+  resources :products do
+    resources :variants, only: [:new, :create]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
