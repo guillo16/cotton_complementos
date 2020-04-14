36a632e373e33679f55e80835da1afed285dd14a
guillermo varela <guillovarela16@gmail.com>
2020-04-14 09:39:10 -0300

    create the controllers

---
+++ app/controllers/variants_controller.rb
@@ -0,0 +1,7 @@
+class VariantsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'variants/new'
+  get 'variants/create'
+  get 'products/index'
+  get 'products/show'
+  get 'products/new'
+  get 'products/create'
+  get 'products/update'
+  get 'products/edit'
+  get 'products/destroy'
+  get 'categories/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
