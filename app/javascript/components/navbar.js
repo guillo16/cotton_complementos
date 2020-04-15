f53722c4a68a29a2aff65fc708d7fba8d9be452c
guillermo varela <guillovarela16@gmail.com>
2020-04-15 19:01:48 -0300

    finish the navbar

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-home');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= window.innerHeight * 0.9) {
         navbar.classList.add('navbar-home-white');
       } else {
         navbar.classList.remove('navbar-home-white');
