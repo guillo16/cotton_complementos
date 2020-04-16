fc1533ea9d4ef20af22cb2033079beccefa0eafc
guillermo varela <guillovarela16@gmail.com>
2020-04-16 14:28:57 -0300

    new variant added

--- app/controllers/variants_controller.rb
+++ app/controllers/variants_controller.rb
@@ -1,7 +1,24 @@
 class VariantsController < ApplicationController
-  def new
+  def create
+    @product = Product.find(params[:product_id])
+    @variant = Variant.new(variant_params)
+    @variant.product = @product
+    if @variant.save
+      redirect_to product_path(@product)
+    else
+      render 'products/show'
+    end
   end
 
-  def create
+  def destroy
+    @variant = Variant.find(params[:id])
+    @variant.destroy
+    redirect_to products_path
+  end
+
+  private
+
+  def variant_params
+    params.require(:variant).permit(:title, :stock)
   end
 end
