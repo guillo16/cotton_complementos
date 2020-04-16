5d715d44b451e1669c07686e914e5158b2cb81ba
guillermo varela <guillovarela16@gmail.com>
2020-04-16 15:33:00 -0300

    add new migration

---
+++ db/migrate/20200416183206_add_price_to_product.rb
@@ -0,0 +1,6 @@
+class AddPriceToProduct < ActiveRecord::Migration[5.2]
+  def change
+    add_monetize :products, :price, currency: { present: false }
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_164829) do
+ActiveRecord::Schema.define(version: 2020_04_16_183206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_04_16_164829) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price_cents", default: 0, null: false
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
