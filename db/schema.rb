578cb778e682487ad80c776b5f9c51ce7bc5f6fe
guillermo varela <guillovarela16@gmail.com>
2020-04-16 13:45:02 -0300

    add models cart and line items

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_181725) do
+ActiveRecord::Schema.define(version: 2020_04_16_164406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -29,6 +34,17 @@ ActiveRecord::Schema.define(version: 2020_04_14_181725) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.bigint "cart_id"
+    t.bigint "product_id"
+    t.bigint "variant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
+    t.index ["variant_id"], name: "index_line_items_on_variant_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -62,6 +78,9 @@ ActiveRecord::Schema.define(version: 2020_04_14_181725) do
   end
 
   add_foreign_key "categories", "divisions"
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "products"
+  add_foreign_key "line_items", "variants"
   add_foreign_key "products", "categories"
   add_foreign_key "variants", "products"
 end
