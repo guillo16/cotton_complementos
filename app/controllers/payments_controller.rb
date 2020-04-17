fac6bace3bf862d0ced25a421fecd9f3bf2ec27e
guillermo varela <guillovarela16@gmail.com>
2020-04-17 10:44:54 -0300

    finish logic of payments and order

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,20 @@
 class PaymentsController < ApplicationController
-  def new
+ before_action :set_order
+
+ def new
+ end
+
+ def create
+    # DESTROY CART
+    @cart = Cart.find(session[:cart_id])
+    session[:cart_id] = nil
+    @order.update(state: 'Encargado')
+    redirect_to order_path(@order)
+  end
+
+  private
+
+  def set_order
+    @order = current_user.orders.where(state: 'Pendientes').find(params[:order_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'payments/new'
   devise_for :users
   root to: 'pages#home'
+  post 'procesar-pago', to: "payments#create"
   resources :divisions, only: :show
   resources :categories, only: :show
   resources :products do
@@ -10,7 +11,9 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
 
-  resources :orders, only: [:show, :create]
+  resources :orders, only: [:index, :show, :create] do
+    resources :payments, only: :new
+  end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
