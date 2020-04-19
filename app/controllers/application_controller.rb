19f06c0127f5fec4029ca685ba76c7c2c052f78b
guillermo varela <guillovarela16@gmail.com>
2020-04-19 11:56:27 -0300

    Create set product in main controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_division
+
+  private
+
+  def set_division
+    @divisions = Division.all
+  end
 end
