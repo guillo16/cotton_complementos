5a9d7e11831dc9b48a9a04beb1e04c914d16446c
guillermo varela <guillovarela16@gmail.com>
2020-04-20 12:04:33 -0300

    Add new fields to form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,17 @@ class ApplicationController < ActionController::Base
   before_action :set_division
   include CurrentCart
   before_action :set_cart
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def set_division
     @divisions = Division.all
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name, :address, :city, :state, :postal, :phone])
   end
 end
