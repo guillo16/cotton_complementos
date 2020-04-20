f4c764728907cb5ef5176d3b40d17d1ec83f4e36
guillermo varela <guillovarela16@gmail.com>
2020-04-20 14:17:39 -0300

    Finish new fields added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,5 +15,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name, :address, :city, :state, :postal, :phone])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :last_name, :address, :city, :state, :postal, :phone])
   end
 end
