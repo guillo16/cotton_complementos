11bfd4ed609bf903c756e565b177e2addf5435ed
guillermo varela <guillovarela16@gmail.com>
2020-04-30 19:47:41 -0300

    Finish the Mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
 
