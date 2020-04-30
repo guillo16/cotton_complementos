4e2ebd468419a18cc03fd87741fa56efc909a184
guillermo varela <guillovarela16@gmail.com>
2020-04-30 16:58:11 -0300

    Emails not sending

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
+
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
-  config.action_mailer.delivery_method = :letter_opener
 
   # Settings specified here will take precedence over those in config/application.rb.
 
