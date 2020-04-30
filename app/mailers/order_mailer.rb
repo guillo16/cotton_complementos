0e5af46e412aa067f1cd3e22818ca8befa8f014f
guillermo varela <guillovarela16@gmail.com>
2020-04-30 16:17:55 -0300

    Finish the order email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,6 +2,6 @@ class OrderMailer < ApplicationMailer
   def new_order_email
     @order = params[:order]
 
-    mail(to: ENV['ADMIN_EMAIL'], subject: "Tienes una nueva orden!")
+    mail(to: ENV["ADMIN_EMAIL"], subject: "Tienes una nueva orden!")
   end
 end
--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order_mailer
 class OrderMailerPreview < ActionMailer::Preview
+  def new_order_email
+    # Set up a temporary order for the preview
+    order = Order.new(amount: 203)
 
+    OrderMailer.with(order: order).new_order_email
+  end
 end
