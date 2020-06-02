85ae601703a5ee027bc3b0e56bf1985b215d4c31
guillermo varela <guillovarela16@gmail.com>
2020-06-02 09:17:29 -0300

    Finish charge controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,10 +1,16 @@
 class ChargesController < ApplicationController
   def index
+    @charges = Charge.all
   end
 
   def show
+    @charge = Charge.find(params[:id])
   end
 
   def create
+    product = Product.find(params[:product_id])
+    Charge.create!(product: product, user: current_user)
+    redirect_to root_path
+    flash[:notice] = "Gracias por su mensaje, lo contactaremos pronto"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,7 @@ Rails.application.routes.draw do
   end
   resources :variants, only: [:show, :edit, :update]
   resources :users, only: [:index, :show]
+
+  resources :charges, only: [:index, :show, :create]
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
