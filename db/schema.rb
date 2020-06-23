aca32c1990444f1c18ab41c0c4e91868bfcb3784
guillermo varela <guillovarela16@gmail.com>
2020-06-23 11:45:46 -0300

    Facebook login migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_121141) do
+ActiveRecord::Schema.define(version: 2020_06_23_144510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,11 @@ ActiveRecord::Schema.define(version: 2020_06_02_121141) do
     t.string "postal"
     t.string "city"
     t.string "state"
+    t.string "provider"
+    t.string "uid"
+    t.string "facebook_picture_url"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
