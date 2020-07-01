dba9554c077fae7ffde7a2862b8b0f92cde3ade2
guillermo varela <guillovarela16@gmail.com>
2020-07-01 14:57:03 -0300

    facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,25 +9,12 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :validatable
   devise :omniauthable, omniauth_providers: [:facebook]
 
-  def self.find_for_facebook_oauth(auth)
-    user_params = auth.slice("provider", "uid")
-    user_params.merge! auth.info.slice("email", "name", "last_name")
-    user_params[:facebook_picture_url] = auth.info.image
-    user_params[:token] = auth.credentials.token
-    user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
-    user_params = user_params.to_h
-
-    user = User.find_by(provider: auth.provider, uid: auth.uid)
-    user ||= User.find_by(email: auth.info.email) # User did a regular sign up in the past.
-    if user
-      user.update(user_params)
-    else
-      user = User.new(user_params)
-      user.password = Devise.friendly_token[0,20] # Fake password for validation
-      user.save
-    end
-
-    return user
+  def self.create_from_provider_data(provider_data)
+     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | user |
+        user.email = provider_data.info.email
+        user.password = Devise.friendly_token[0, 20]
+        user.skip_confirmation!
+      end
   end
 
   private
