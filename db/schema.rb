c34589baef13f4277094be66044965f5e6b28459
guillermo varela <guillovarela16@gmail.com>
2020-08-17 10:41:32 -0300

    Add widht to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_144510) do
+ActiveRecord::Schema.define(version: 2020_08_17_134017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 2020_06_23_144510) do
     t.integer "price_cents", default: 0, null: false
     t.text "description"
     t.integer "sale"
+    t.string "width"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
